ac62baf4e727a00049bf0d6093aca941c47704c3
Raphael BALBOUS <rbalbous@e3r5p5.42.fr>
2018-09-05 21:12:59 +0200

    ok

--- includes/npuzzle.h
+++ includes/npuzzle.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   npuzzle.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afoures <afoures@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rbalbous <rbalbous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 17:14:02 by afoures           #+#    #+#             */
-/*   Updated: 2018/09/05 17:56:18 by afoures          ###   ########.fr       */
+/*   Updated: 2018/09/05 20:33:06 by rbalbous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
  #define NPUZZLE_H
 
 typedef struct s_board	t_board;
+typedef struct s_point	t_point;
+
+struct		s_point
+{
+	int x;
+	int y;
+};
 
 struct		s_board
 {
+	t_point *sol;
 	int		*board;
 	int		*distances;
 	int		size;
