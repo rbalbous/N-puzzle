2c4250767882f2d9306f5a89cbf9cf5730c35e1c
Antoine FOURES <afoures@e3r5p7.42.fr>
2018-09-05 22:46:52 +0200

    gen

--- srcs/n-puzzle/main.c
+++ srcs/n-puzzle/main.c
@@ -1,35 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   npuzzle.h                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afoures <afoures@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/05 17:14:02 by afoures           #+#    #+#             */
-/*   Updated: 2018/09/05 21:19:50 by afoures          ###   ########.fr       */
+/*   Created: 2018/09/05 21:20:09 by afoures           #+#    #+#             */
+/*   Updated: 2018/09/05 22:37:01 by afoures          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef NPUZZLE_H
- #define NPUZZLE_H
+#include "npuzzle.h"
 
- #include "ft_printf.h"
-
-typedef struct s_board	t_board;
-typedef struct s_point	t_point;
-
-struct		s_point
+int		main(int ac, char *av[])
 {
-	int x;
-	int y;
-};
-
-struct		s_board
-{
-	t_point *sol;
-	int		**board;
-	int		**distances;
-	int		size;
-};
-
-#endif
+	if (ac > 1)
+		
+	return (0);
+}
--- srcs/n-puzzle/print.c
+++ srcs/n-puzzle/print.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   npuzzle.h                                          :+:      :+:    :+:   */
+/*   print.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afoures <afoures@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/05 17:14:02 by afoures           #+#    #+#             */
-/*   Updated: 2018/09/05 21:19:50 by afoures          ###   ########.fr       */
+/*   Created: 2018/09/05 21:21:05 by afoures           #+#    #+#             */
+/*   Updated: 2018/09/05 21:48:11 by afoures          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef NPUZZLE_H
- #define NPUZZLE_H
+#include "npuzzle.h"
 
- #include "ft_printf.h"
-
-typedef struct s_board	t_board;
-typedef struct s_point	t_point;
-
-struct		s_point
+void	print_board(t_board *board)
 {
-	int x;
-	int y;
-};
 
-struct		s_board
-{
-	t_point *sol;
-	int		**board;
-	int		**distances;
-	int		size;
-};
-
-#endif
+}
